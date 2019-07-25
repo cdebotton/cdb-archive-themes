c162a1f7fc48efd09f96c98735efdbe2fd369b2b
Christian de Botton <cdebotton@gmail.com>
2019-07-24 21:41:38 -0400

    Test deployment again.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,8 @@ export default function IndexPage() {
   return (
     <>
       <Text scale={4} as="h1">
-        Hello, world!
+        Hello world!
       </Text>
-      <Text>This is my cool website.</Text>
     </>
   );
 }
