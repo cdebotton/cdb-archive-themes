563be08a2be0bc3fdad81584e0c5552b777d4e86
Christian de Botton <cdebotton@gmail.com>
2019-07-28 22:39:13 -0400

    Remove graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,38 @@
 import React from 'react';
-import { Container } from 'next/app';
+import { Container, AppProps } from 'next/app';
 import Head from 'next/head';
 import { css } from 'styled-components';
-import { ApolloProvider } from '@apollo/react-hooks';
 
 import { AppStyle } from '../components/AppStyle';
 import { Viewport } from '../components/Viewport';
 import { ThemeProvider } from '../components/Theme';
 import { SocialAppBar } from '../components/SocialAppBar';
-import withApolloClient, { ApolloAppProps } from '../lib/with-apollo-client';
 
-function App({ Component, pageProps, apolloClient }: ApolloAppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Container>
-        <Head>
-          <title>Christian de Botton</title>
-          <meta
-            name="description"
-            content="Photography portfolio and occasional engineering focused writing by Brooklyn based software engineer, Christian de Botton."
+    <Container>
+      <Head>
+        <title>Christian de Botton</title>
+        <meta
+          name="description"
+          content="Photography portfolio and occasional engineering focused writing by Brooklyn based software engineer, Christian de Botton."
+        />
+      </Head>
+      <ThemeProvider>
+        <Viewport>
+          <AppStyle />
+          <Component {...pageProps} />
+          <SocialAppBar
+            css={css`
+              position: fixed;
+              bottom: 0;
+              right: 0;
+            `}
           />
-        </Head>
-        <ThemeProvider>
-          <Viewport>
-            <AppStyle />
-            <Component {...pageProps} />
-            <SocialAppBar
-              css={css`
-                position: fixed;
-                bottom: 0;
-                right: 0;
-              `}
-            />
-          </Viewport>
-        </ThemeProvider>
-      </Container>
-    </ApolloProvider>
+        </Viewport>
+      </ThemeProvider>
+    </Container>
   );
 }
 
-export default withApolloClient(App);
+export default App;
