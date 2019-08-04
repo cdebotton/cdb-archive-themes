09264e0e10fc0cd91158593e29b4997fc11cbc90
Christian de Botton <cdebotton@gmail.com>
2019-08-04 19:01:49 -0400

    Remove storybook

--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { addDecorator, configure } from '@storybook/react';
-
-// automatically import all files ending in *.stories.js
-configure(
-  require.context('..', true, /(components|pages)\/.+\.story\.tsx$/),
-  module,
-);
