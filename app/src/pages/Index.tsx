72692244350f6e0f5d43641a260f579bba46d09e
Christian de Botton <cdebotton@gmail.com>
2019-08-14 11:21:23 -0400

    Update meta.

--- app/src/pages/Index.tsx
+++ app/src/pages/Index.tsx
@@ -10,7 +10,17 @@ export default function IndexPage() {
       <p>Oh, hello!</p>
       <Heading scale={4}>My name's Christian</Heading>
       <p>
-        I'm a web application engineer at <Vimeo />
+        I'm a web application engineer at{' '}
+        <a target="_BLANK" rel="noopener noreferrer" href="//vimeo.com">
+          <Vimeo />
+        </a>
+        . I currently lead the front-end platform team there, but have
+        previously lead product development for various teams within the
+        organization. Prior to that, I worked at a small agency in Brooklyn as
+        their Director of Engineering, developing marketing driven solutions for
+        companies like Netflix, The Moth, and AT&amp;T. I'm based in Brooklyn,
+        and when I have free time I enjoy wandering around and taking photos
+        with my Fujifilm X-T2.
       </p>
     </Page>
   );
